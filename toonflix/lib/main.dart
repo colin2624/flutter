040239eeb0dcb5d214a5c076226a574926ed6575
haen <142671869+colin2624@users.noreply.github.com>
2024-06-27 23:28:08 +0900

    good

---
+++ toonflix/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text('Hello Flutter!'),
+        ),
+        body: Center(
+          child: Text('Helllo world!'),
+        ),
+      ),
+    );
+  }
+}
